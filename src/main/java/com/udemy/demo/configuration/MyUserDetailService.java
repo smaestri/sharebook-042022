0781a9311fd48987f0e076184814225beeafcfc2
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-01-08 21:45:01 +0100

    Session 67 - JwtController

--- src/main/java/com/udemy/demo/configuration/MyUserDetailService.java
+++ src/main/java/com/udemy/demo/configuration/MyUserDetailService.java
@@ -33,6 +33,14 @@ public class MyUserDetailService implements UserDetailsService {
     public static class UserPrincipal implements UserDetails {
         private User user;
 
+        public User getUser() {
+            return user;
+        }
+
+        public void setUser(User user) {
+            this.user = user;
+        }
+
         public UserPrincipal(User user){
             this.user = user;
         }
---
+++ src/main/java/com/udemy/demo/jwt/JwtUtils.java
@@ -0,0 +1,8 @@
+package com.udemy.demo.jwt;
+
+import com.udemy.demo.configuration.MyUserDetailService;
+
+public class JwtUtils {
+    public String generateToken(MyUserDetailService.UserPrincipal principal) {
+    }
+}
