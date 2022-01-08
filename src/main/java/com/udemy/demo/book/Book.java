9476eb1cd35ffb14c8c8c6d5ab197f85929bea78
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-01-08 17:01:48 +0100

    Session 23 - Finition Controller

--- src/main/java/com/udemy/demo/book/Book.java
+++ src/main/java/com/udemy/demo/book/Book.java
@@ -4,6 +4,15 @@ public class Book {
 
     private String title;
     private Category category;
+    private BookStatus bookStatus;
+
+    public BookStatus getBookStatus() {
+        return bookStatus;
+    }
+
+    public void setBookStatus(BookStatus bookStatus) {
+        this.bookStatus = bookStatus;
+    }
 
     public String getTitle() {
         return title;
--- src/main/java/com/udemy/demo/user/UserController.java
+++ src/main/java/com/udemy/demo/user/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     @PostMapping(value = "/users")
-    public ResponseEntity add(User user) {
+    public ResponseEntity add(@RequestBody User user) {
         User userOld = new User("toto@yopmail.com");
         return new ResponseEntity(userOld, HttpStatus.CREATED);
     }
