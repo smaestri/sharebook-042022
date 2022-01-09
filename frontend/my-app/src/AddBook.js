7e764a5cd7275426082a2cbfea260d561061e7ff
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-01-09 16:16:04 +0100

    Session 98 - Distinct ajout / modifier livre

--- frontend/my-app/src/AddBook.js
+++ frontend/my-app/src/AddBook.js
@@ -1,4 +1,9 @@
+import {useParams} from 'react-router-dom'
 
 export default function AddBook() {
-      return "addBook"
+    let {bookId} = useParams();
+    if(bookId) {
+        return "update book" 
+    }
+    return "addBook"
 }
