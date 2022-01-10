7c91a3f124f299b24d5405f715ccfac78a4ed91f
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-01-10 12:08:09 +0100

    Session 126 - Suppression livre

--- frontend/my-app/src/MyBooks.js
+++ frontend/my-app/src/MyBooks.js
@@ -3,34 +3,45 @@ import axios from 'axios';
 import { Link } from 'react-router-dom'
 import Book from './Book'
 
+
 import './MyBooks.scss'
 
 const MyBooks = () => {
 
-  const [myBooks, setMyBooks] = React.useState([])
-  
-    React.useEffect(() => {
-      axios.get('/books').then(response => {
-        setMyBooks(response.data)
-      })
-    }, [])
+const [myBooks, setMyBooks] = React.useState([])
+
+const fetchBooks = () => {
+  axios.get('/books').then(response => {
+    setMyBooks(response.data)
+  })
+}
+
+  React.useEffect(() => {
+    fetchBooks();
+  }, [])
+
+  const handleDelete = (bookId) => {
+    axios.delete(`/books/${bookId}`).then(response => {
+      fetchBooks();
+    })
+  }
 
   return (
     <div className="container">
-      <h1>Mes livres</h1>
-      <div className="list-container">
-        {myBooks.length === 0 ? "Vous n'avez pas déclaré de livres" : null}
-        {myBooks.map((book, key) => (<div key={key} className="mybook-container">
-          <Book title={book.title} category={book.category.label}></Book>
-          <div className="container-buttons">
-            <Link to={`/addBook/${book.id}`}>
-              <button className="btn btn-primary btn-sm">Modifier</button>
-            </Link>
-            <button className="btn btn-primary btn-sm">Supprimer</button>
-          </div>
-        </div>))}
-      </div>
-      <Link to="/addBook"><button className="btn btn-primary btn-sm">Nouveau livre</button></Link>
-    </div>)
+    <h1>Mes livres</h1>
+    <div className="list-container">
+      {myBooks.length === 0 ? "Vous n'avez pas déclaré de livres" : null}
+      {myBooks.map((book, key) => (<div key={key} className="mybook-container">
+        <Book title={book.title} category={book.category.label}></Book>
+        <div className="container-buttons">
+          <Link to={`/addBook/${book.id}`}>
+            <button className="btn btn-primary btn-sm">Modifier</button>
+          </Link>
+          <button className="btn btn-primary btn-sm" onClick={()=>handleDelete(book.id)}>Supprimer</button>
+        </div>
+      </div>))}
+    </div>
+    <Link to="/addBook"><button className="btn btn-primary btn-sm">Nouveau livre</button></Link>
+  </div>)
 }
 export default MyBooks;
